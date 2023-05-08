dff149500341dc58008973ba229c739782cff392
Snojj25 <snoj.jure1@gmail.com>
2023-05-08 19:00:06 +0200

    wasm build pkg

---
+++ pkg/starknet.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} a
+* @param {string} b
+* @returns {string}
+*/
+export function pedersen_binding(a: string, b: string): string;
+/**
+* @param {any[]} arr
+* @returns {string}
+*/
+export function pedersen_on_vec_binding(arr: any[]): string;
--- test.js
+++ test.js
@@ -19,7 +19,10 @@ const pedersen_hash = starknet_hash_utils.hash.pedersen;
 let a = 123459253786237523535892357235532n;
 let b = 389625662538953258923568235623533n;
 
-let h = starkwareCryptoSys.pedersen_binding("123459253786237523535892357235532", "389625662538953258923568235623533");
+let h = starkwareCryptoSys.pedersen_binding(
+  "123459253786237523535892357235532",
+  "389625662538953258923568235623533"
+);
 
 let check = pedersen_hash([a, b]);
 
