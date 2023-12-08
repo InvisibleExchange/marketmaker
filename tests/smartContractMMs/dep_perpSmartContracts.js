b8a54fe85116a1344e80fc79a94f3980f57e2e38
Snojj25 <snoj.jure1@gmail.com>
2023-12-08 20:53:38 +0100

    .

---
+++ tests/smartContractMMs/dep_perpSmartContracts.js
@@ -0,0 +1,177 @@
+const { makeDeposits } = require("../../helpers");
+
+const { UserState } = require("invisible-sdk/src/users");
+const {
+  COLLATERAL_TOKEN_DECIMALS,
+  PERP_MARKET_IDS_2_TOKENS,
+} = require("invisible-sdk/src/helpers/utils");
+const {
+  sendRegisterMm,
+  sendAddLiquidityUser,
+  sendOnChainAddLiquidityMM,
+  sendOnChainRemoveLiquidityUser,
+  sendOnChainRemoveLiquidityMM,
+  sendPerpOrder,
+} = require("invisible-sdk/src/transactions");
+
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const { UserState } = require("invisible-sdk/src/users");
+const { computeHashOnElements } = require("invisible-sdk/src/utils");
+
+const packageDefinition = protoLoader.loadSync("../engine.proto", {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+const engine = grpc.loadPackageDefinition(packageDefinition).engine;
+
+const SERVER_URL = "localhost:50052";
+
+let client = new engine.Engine(SERVER_URL, grpc.credentials.createInsecure());
+
+//
+
+async function initMM() {
+  let privKey = 101248239572738957238572395803135238950951n;
+
+  //
+  await makeDeposits([55555], [20_000], privKey);
+}
+
+async function initPosition() {
+  let privKey = 101248239572738957238572395803135238950951n;
+  let marketMaker = await UserState.loginUser(privKey);
+
+  await sendPerpOrder(
+    marketMaker,
+    "Long",
+    1000,
+    "Open",
+    null,
+    54321,
+    0.05,
+    1650,
+    20_000,
+    0.07,
+    1,
+    true,
+    null
+  );
+}
+
+async function registerMM() {
+  let privKey = 101248239572738957238572395803135238950951n;
+  let marketMaker = await UserState.loginUser(privKey);
+
+  let baseToken = PERP_MARKET_IDS_2_TOKENS[22];
+  let position = marketMaker.positionData[baseToken][0];
+
+  console.log("position before", position);
+
+  let vlpToken = 13579;
+  let maxVlpSupply = 1_000_000;
+
+  // GrpcPerpPosition position = 1;
+  // uint32 vlp_token = 2;
+  // uint64 max_vlp_supply = 3;
+  // Signature signature = 4;
+  // uint32 market_id = 5;
+  // uint32 synthetic_token = 6;
+
+  await sendRegisterMm(
+    marketMaker,
+    vlpToken,
+    maxVlpSupply,
+    position.position_header.position_address,
+    true,
+    22
+  );
+}
+
+async function addLiquidity() {
+  // ? MARKET MAKER
+  let privKey = 101248239572738957238572395803135238950951n;
+  let marketMaker = await UserState.loginUser(privKey);
+
+  let baseToken = PERP_MARKET_IDS_2_TOKENS[22];
+  let position = marketMaker.positionData[baseToken][0];
+  console.log("position before", position);
+
+  // ? USER
+  privKey = 22222n;
+
+  // await makeDeposits([55555], [5_000], userConfig);
+
+  let user = await UserState.loginUser(privKey);
+  if (user.getAvailableAmount(55555) == 0) {
+    await makeDeposits([55555], [5_000], privKey);
+
+    user = await UserState.loginUser(privKey);
+  }
+
+  console.log("user quote amount", user.getAvailableAmount(55555));
+
+  let vlpToken = 13579;
+
+  let collateralAmount =
+    user.getAvailableAmount(55555) / 10 ** COLLATERAL_TOKEN_DECIMALS;
+
+  let res = await sendAddLiquidityUser(
+    user,
+    position.position_header.position_address,
+    vlpToken,
+    null,
+    null,
+    collateralAmount,
+    22,
+    true
+  );
+
+  await sendOnChainAddLiquidityMM(marketMaker, res);
+}
+
+async function removeLiquidity() {
+  // ? MARKET MAKER
+  let privKey = 101248239572738957238572395803135238950951n;
+  let marketMaker = await UserState.loginUser(privKey);
+
+  let baseToken = PERP_MARKET_IDS_2_TOKENS[22];
+  let position = marketMaker.positionData[baseToken][0];
+  console.log("position before", position);
+
+  // ? USER
+  privKey = 22222n;
+  let user = await UserState.loginUser(privKey);
+
+  let vlpToken = 13579;
+
+  let vlpBalance = user.getAvailableAmount(vlpToken);
+  console.log("user vlp balance", vlpBalance);
+
+  let grpcMessage = await sendOnChainRemoveLiquidityUser(
+    user,
+    position.position_header.position_address,
+    vlpToken,
+    null,
+    null,
+    22,
+    true
+  );
+
+  // console.log("grpcMessage", grpcMessage);
+
+  await sendOnChainRemoveLiquidityMM(marketMaker, grpcMessage);
+}
+
+async function main() {
+  // await initMM();
+  // await initPosition();
+  // await registerMM();
+  // await addLiquidity();
+  // await removeLiquidity();
+}
+
+main();
--- tests/smartContractMMs/perpSmatContractMM.js
+++ tests/smartContractMMs/perpSmatContractMM.js
@@ -1,18 +1,27 @@
-const { makeDeposits } = require("../../helpers");
+const { makeDeposits } = require("../../src/helpers");
 
+const { sign, getKeyPair } = require("starknet").ec;
+
+const { PERP_MARKET_IDS_2_TOKENS } = require("invisible-sdk/src/utils");
+const { sendPerpOrder } = require("invisible-sdk/src/transactions");
+
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
 const { UserState } = require("invisible-sdk/src/users");
-const {
-  COLLATERAL_TOKEN_DECIMALS,
-  PERP_MARKET_IDS_2_TOKENS,
-} = require("invisible-sdk/src/helpers/utils");
-const {
-  sendRegisterMm,
-  sendAddLiquidityUser,
-  sendOnChainAddLiquidityMM,
-  sendOnChainRemoveLiquidityUser,
-  sendOnChainRemoveLiquidityMM,
-  sendPerpOrder,
-} = require("invisible-sdk/src/transactions");
+const { computeHashOnElements } = require("invisible-sdk/src/utils");
+
+const packageDefinition = protoLoader.loadSync("../engine.proto", {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+const engine = grpc.loadPackageDefinition(packageDefinition).engine;
+
+const SERVER_URL = "localhost:50052";
+
+let client = new engine.Engine(SERVER_URL, grpc.credentials.createInsecure());
 
 //
 
@@ -35,35 +44,58 @@ async function initPosition() {
     null,
     54321,
     0.05,
-    1650,
+    2350,
     20_000,
     0.07,
     1,
     true,
     null
   );
+
+  console.log("positionData", marketMaker.positionData[54321]);
 }
 
 async function registerMM() {
   let privKey = 101248239572738957238572395803135238950951n;
   let marketMaker = await UserState.loginUser(privKey);
 
-  let baseToken = PERP_MARKET_IDS_2_TOKENS[22];
-  let position = marketMaker.positionData[baseToken][0];
+  let syntheticToken = PERP_MARKET_IDS_2_TOKENS[22];
+  let position = marketMaker.positionData[syntheticToken][0];
+  position.order_side = position.order_side === "Long";
 
-  console.log("position before", position);
+  let posPrivKey =
+    marketMaker.positionPrivKeys[position.position_header.position_address];
 
   let vlpToken = 13579;
-  let maxVlpSupply = 1_000_000;
+  let maxVlpSupply = 1_000_000_000_000;
 
-  await sendRegisterMm(
-    marketMaker,
+  // & H = H({position.hash, vlp_token, max_vlp_supply})
+  let messageHash = computeHashOnElements([
+    position.hash,
     vlpToken,
     maxVlpSupply,
-    position.position_header.position_address,
-    true,
-    22
-  );
+  ]);
+
+  let keyPair = getKeyPair(posPrivKey);
+  let sig = sign(keyPair, "0x" + messageHash.toString(16));
+  let marketId = 22;
+
+  let registerMessage = {
+    position: position,
+    vlp_token: vlpToken,
+    max_vlp_supply: maxVlpSupply,
+    signature: { r: sig[0], s: sig[1] },
+    market_id: marketId,
+    synthetic_token: syntheticToken,
+  };
+
+  await client.register_onchain_mm(registerMessage, function (err, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("response", response);
+    }
+  });
 }
 
 async function addLiquidity() {
@@ -71,41 +103,21 @@ async function addLiquidity() {
   let privKey = 101248239572738957238572395803135238950951n;
   let marketMaker = await UserState.loginUser(privKey);
 
-  let baseToken = PERP_MARKET_IDS_2_TOKENS[22];
-  let position = marketMaker.positionData[baseToken][0];
-  console.log("position before", position);
+  let syntheticToken = PERP_MARKET_IDS_2_TOKENS[22];
+  let position = marketMaker.positionData[syntheticToken][0];
+  position.order_side = position.order_side === "Long";
 
-  // ? USER
-  privKey = 22222n;
+  let posPrivKey =
+    marketMaker.positionPrivKeys[position.position_header.position_address];
 
-  // await makeDeposits([55555], [5_000], userConfig);
-
-  let user = await UserState.loginUser(privKey);
-  if (user.getAvailableAmount(55555) == 0) {
-    await makeDeposits([55555], [5_000], privKey);
-
-    user = await UserState.loginUser(privKey);
-  }
-
-  console.log("user quote amount", user.getAvailableAmount(55555));
-
-  let vlpToken = 13579;
-
-  let collateralAmount =
-    user.getAvailableAmount(55555) / 10 ** COLLATERAL_TOKEN_DECIMALS;
-
-  let res = await sendAddLiquidityUser(
-    user,
-    position.position_header.position_address,
-    vlpToken,
-    null,
-    null,
-    collateralAmount,
-    22,
-    true
-  );
-
-  await sendOnChainAddLiquidityMM(marketMaker, res);
+  // message OnChainAddLiqReq {
+  //   GrpcPerpPosition position = 1;
+  //   string depositor = 2;
+  //   uint64 initial_value = 3;
+  //   Signature signature = 4;
+  //   uint32 market_id = 5;
+  //   uint32 synthetic_token = 6;
+  // }
 }
 
 async function removeLiquidity() {
@@ -116,37 +128,56 @@ async function removeLiquidity() {
   let baseToken = PERP_MARKET_IDS_2_TOKENS[22];
   let position = marketMaker.positionData[baseToken][0];
   console.log("position before", position);
-
-  // ? USER
-  privKey = 22222n;
-  let user = await UserState.loginUser(privKey);
-
-  let vlpToken = 13579;
-
-  let vlpBalance = user.getAvailableAmount(vlpToken);
-  console.log("user vlp balance", vlpBalance);
-
-  let grpcMessage = await sendOnChainRemoveLiquidityUser(
-    user,
-    position.position_header.position_address,
-    vlpToken,
-    null,
-    null,
-    22,
-    true
-  );
-
-  // console.log("grpcMessage", grpcMessage);
-
-  await sendOnChainRemoveLiquidityMM(marketMaker, grpcMessage);
 }
 
 async function main() {
   // await initMM();
+
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+
   // await initPosition();
+
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+
   // await registerMM();
-  // await addLiquidity();
+
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  await addLiquidity();
+
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+
   // await removeLiquidity();
 }
 
 main();
+
+// Ok(PerpPosition {
+// index: 3, position_header:
+// PositionHeader { synthetic_token: 54321, position_address: 2515621936646526414291941246684465506968430707280856084433062674401974871214,
+// allow_partial_liquidations: true, vlp_token: 0, max_vlp_supply: 0,
+
+// hash: 1485521988648034206478844620559802264092962755549123833095291387531899750380 }, order_side: Short,
+// position_size: 5000000, margin: 19999941345, entry_price: 2320000000, liquidation_price: 0, bankruptcy_price: 0,
+// last_funding_idx: 0, vlp_supply: 0, hash: 3339438903260205812244036825303405518899419210941861089465262344458033609107 })
+
+// position before {
+//   hash: '619393722529871128088058264914784089171038657038758816997537177050323159895',
+//   bankruptcy_price: 0,
+//   last_funding_idx: 0,
+//   entry_price: 2320000000,
+//   order_side: 'Long',
+//   position_size: 5000000,
+//   index: 3,
+//   vlp_supply: 0,
+//   position_header: {
+//     allow_partial_liquidations: true,
+//     vlp_token: 0,
+//     synthetic_token: 54321,
+//     hash: '1485521988648034206478844620559802264092962755549123833095291387531899750380',
+//     position_address: '2515621936646526414291941246684465506968430707280856084433062674401974871214',
+//     max_vlp_supply: 0
+//   },
+//   liquidation_price: 0,
+//   margin: 19999941345
+// }
